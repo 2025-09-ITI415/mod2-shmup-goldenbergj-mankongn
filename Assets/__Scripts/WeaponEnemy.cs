ffe339079838e6bd0fd5a67d98dcb50cc6f93170
J Goldenberg <jag629@scarletmail.rutgers.edu>
2025-11-06 13:59:53 -0500

    Allowed enemies to shoot projectiles
    WeaponEnemy.cs allows enemies to have a weapon and fire every 2 seconds by default. Enemy_0 prefab given weapon object & ShotAnchor script, currently damaging itself with hero projectiles.

--- Assets/__Scripts/WeaponEnemy.cs
+++ Assets/__Scripts/WeaponEnemy.cs
@@ -4,14 +4,40 @@ using UnityEngine;
 public class WeaponEnemy : MonoBehaviour
 {
     public Weapon weapon;
-    void Start()
+    public bool weaponCooldown = true;
+    public float fireEvery = 2f;
+    public float initialDelay = 0.5f;
+
+    float nextFireAt;
+    Enemy enemy;
+
+    void Awake()
     {
-        
+        enemy = GetComponent<Enemy>();
+        if (weapon == null) weapon = GetComponentInChildren<Weapon>();
     }
 
-    // Update is called once per frame
+    void OnEnable()
+    {
+        nextFireAt = Time.time + Random.Range(0f, initialDelay);
+    }
+
     void Update()
     {
-        
+        if (weapon == null) return;
+
+        if (weaponCooldown)
+        {
+            if (Time.time >= nextFireAt) {
+                weapon.TryFire();
+                nextFireAt = Time.time + 0.05f;
+            }
+        } else
+        {
+            if (Time.time >= nextFireAt) {
+                weapon.TryFire();
+                nextFireAt = Time.time + Mathf.Max(0.05f, fireEvery);
+            }
+        }
     }
 }
